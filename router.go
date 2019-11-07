7669f8c109d622cbdb8d383af62c470b6e3c17fc
Pablo Frias <pafrias90@gmail.com>
2019-11-06 21:11:24 -0800

    Minor name change in app/

--- router.go
+++ router.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"github.com/pafrias/2cgaming-api/app/trap"
 
@@ -27,7 +29,7 @@ func (s *server) createMainRouter() {
 }
 
 func (s *server) applyTrapCompendiumRoutes(r *mux.Router) {
-	trapAPI := trap.TCHandler{s.db}
+	trapAPI := trap.NewHandler(s.db)
 	r.HandleFunc("/test", trapAPI.PrintForm()).Methods("POST")
 
 	r.HandleFunc("/components", trapAPI.GetComponents()).Methods("GET")
@@ -40,13 +42,50 @@ func (s *server) applyTrapCompendiumRoutes(r *mux.Router) {
 	// PATCH NEEDED
 }
 
-// func spell codex api routes
-
 func (s *server) serveStaticFiles() http.HandlerFunc {
 	dir := http.Dir("./web")
 	fs := http.FileServer(dir)
 	return func(res http.ResponseWriter, req *http.Request) {
-		fmt.Println("getting file -> ", req.URL)
-		fs.ServeHTTP(res, req)
+		path := req.RequestURI
+		if s.spaTest(path, "/trapcompendium/") {
+			filePath := "./web/trapcompendium/"
+			fileName, Type := s.readFileExtension(path)
+			filePath += fileName
+			fmt.Printf("redirected request from '%v' to '%v'\n", path, filePath)
+			file, err := ioutil.ReadFile(filePath)
+			if err != nil {
+				fmt.Printf("something happened when trying to reach: %v\n", path)
+			} else {
+				if Type != "" {
+					res.Header().Set("Content-Type", "text/"+Type)
+				}
+				res.Write(file)
+			}
+		} else {
+			fs.ServeHTTP(res, req)
+		}
+	}
+}
+
+// helps routing for single page applications
+func (s *server) spaTest(path string, prefix string) bool {
+	if strings.HasPrefix(path, prefix) {
+		fmt.Printf("prefix '%v' matches path '%v'\n", path, prefix)
+		return true //strings.HasSuffix(path, "/") || strings.HasSuffix(path, "/index.html")
+	}
+	return false
+}
+
+func (s *server) readFileExtension(path string) (string, string) {
+	pathVals := strings.Split(path, "/")
+	switch file := pathVals[len(pathVals)-1]; {
+	case file == "index.js" || file == "":
+		return "index.html", "html"
+	case file == "main.js":
+		return "main.js", ""
+	case strings.HasSuffix(file, ".css"):
+		return "css/" + file, "css"
+	default:
+		return "media/" + file, ""
 	}
 }
